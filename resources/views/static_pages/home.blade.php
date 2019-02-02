d9478e1e6fdb792e134fa0827cc6f7f3300d894f
Sian Chou <webcodedemo@gmail.com>
2019-02-02 13:41:36 +0000

    初始化樣式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,11 @@
 @extends('layouts.default')
+
 @section('content')
-  <h1>主頁</h1>
+  <div class="jumbotron">
+  <h1>Hello Laravel</h1>
+  <p class="lead">你現在所在的是Weibo App主頁</p>
+  <p>歡迎加入</p>
+  <p>
+    <a class="btn btn-lg btn-success" href="#" role="button">註冊</a>
+  </p>
 @stop
