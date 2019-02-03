3fe04d89c6f039b5f998e6f2d4b68b700d8dc8a8
Sian Chou <webcodedemo@gmail.com>
2019-02-03 01:39:23 +0000

    登入時記住我 & 修正程式碼

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -15,27 +15,24 @@ class SessionsController extends Controller
 
     public function store(Request $request)
     {
-        $this->validate($request, [
-            'name' => 'required|max:50',
-            'email' => 'required|email|unique:users|max:255',
-            'password' => 'required|confirmed|min:6'
+        $credentials = $this->validate($request, [
+           'email' => 'required|email|max:255',
+           'password' => 'required'
         ]);
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => bcrypt($request->password),
-        ]);
-
-        Auth::login($user);
-        session()->flash('success', '成功登入');
-        return redirect()->route('users.show', [$user->id]);
+        if (Auth::attempt($credentials, $request->has('remember'))) {
+            session()->flash('success', '歡迎登入');
+            return redirect()->route('users.show', [Auth::user()]);
+        } else {
+            session()->flash('danger', '很抱歉，你的電子信箱或密碼有錯誤');
+            return redirect()->back()->withInput();
+        }
     }
 
     public function destroy()
     {
         Auth::logout();
-        session()->flash('success', '你已成功退出');
+        session()->flash('success', '你已成功登出');
         return redirect('login');
     }
 }
