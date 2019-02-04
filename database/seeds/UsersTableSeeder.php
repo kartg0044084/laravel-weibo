d3bbf0c28949aadbeaf588b2dbc6b6fdfdb30cf8
Sian Chou <webcodedemo@gmail.com>
2019-02-04 06:52:08 +0000

    管理員可刪除使用者

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,6 +13,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'cora';
         $user->email = 'cora@gmail.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
